a376df579121515c85a2217d24944f28636aacc0
Nikhil Rai <nikhilrai637@gmail.com>
2020-08-14 00:21:57 +0530

    Just added h1 tag on page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+       <h1>Home Page</h1>
       </header>
     </div>
   );
